c2220559f429d9ff4d061d92cd8377ec66b5ef99
Fabio-Case <fabiocasetta.25@gmail.com>
2019-11-24 12:03:15 +0100

    agiiunta dll probabilità e imprevisti

---
+++ Monopoli/Imprevisti e probabiltà/CElementi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAzioneInvolontariaObbligatoria
+{
+    class CElementi
+    {
+        private string _cartellino;
+        private string _posizione;
+        private int _denaro;
+
+        public CElementi()
+        {
+            _cartellino = "";
+            _posizione = "";
+            _denaro = 0;
+        }
+
+        public CElementi(string cartellino,int denaro,string posizione)
+        {
+            this._cartellino = cartellino;
+            this._denaro = denaro;
+            this._posizione = posizione;
+        }
+
+        public int denaro
+        {
+            get { return _denaro; }
+        }
+
+        public String cartellino
+        {
+            get { return _cartellino; }
+        }
+
+        public String posizione
+        {
+            get { return _posizione; }
+        }
+
+
+    }
+}
